dd53cefef94da871b152c4b9b2c0efb2690503fd
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2022-10-01 08:59:32 +0530

    An optimized version of Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,8 +13,31 @@ public class BubbleSortExample {
                           
                  }  
          }  
+        
   
-    }  
+    } 
+    // An optimized version of Bubble Sort
+    static void optimizedbubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+    }
     public static void main(String[] args) {  
                 int arr[] ={3,60,35,2,45,320,5};  
                  
