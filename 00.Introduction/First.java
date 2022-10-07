c86cd037ac581335ae8a875b56e87c37cd3f9c1a
Aditi Ghate <107868821+Adiana3308@users.noreply.github.com>
2022-10-07 21:59:58 +0530

    Introduction to Java Programming.

---
+++ 00.Introduction/First.java
@@ -0,0 +1,9 @@
+class First
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+        // System.out.println(args[0]);
+        // System.out.println(args[1]);
+    }
+}
