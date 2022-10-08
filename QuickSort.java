e7fa2ace70cfab169edce0075833d10506749922
Saurav Sutaria <73012044+Saurav-Sutaria@users.noreply.github.com>
2022-10-08 21:34:37 +0530

    Added Quick sort program with time analysis

--- QuickSort.java
+++ QuickSort.java
@@ -1,85 +1,39 @@
-// Java implementation of QuickSort
-import java.io.*;
-
-class GFG {
-
-	// A utility function to swap two elements
-	static void swap(int[] arr, int i, int j)
-	{
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
-
-	/* This function takes last element as pivot, places
-	the pivot element at its correct position in sorted
-	array, and places all smaller (smaller than pivot)
-	to left of pivot and all greater elements to right
-	of pivot */
-	static int partition(int[] arr, int low, int high)
-	{
-
-		// pivot
-		int pivot = arr[high];
-
-		// Index of smaller element and
-		// indicates the right position
-		// of pivot found so far
-		int i = (low - 1);
-
-		for (int j = low; j <= high - 1; j++) {
-
-			// If current element is smaller
-			// than the pivot
-			if (arr[j] < pivot) {
-
-				// Increment index of
-				// smaller element
-				i++;
-				swap(arr, i, j);
-			}
-		}
-		swap(arr, i + 1, high);
-		return (i + 1);
-	}
-
-	/* The main function that implements QuickSort
-			arr[] --> Array to be sorted,
-			low --> Starting index,
-			high --> Ending index
-	*/
-	static void quickSort(int[] arr, int low, int high)
-	{
-		if (low < high) {
-
-			// pi is partitioning index, arr[p]
-			// is now at right place
-			int pi = partition(arr, low, high);
-
-			// Separately sort elements before
-			// partition and after partition
-			quickSort(arr, low, pi - 1);
-			quickSort(arr, pi + 1, high);
-		}
-	}
-
-	// Function to print an array
-	static void printArray(int[] arr, int size)
-	{
-		for (int i = 0; i < size; i++)
-			System.out.print(arr[i] + " ");
-
-		System.out.println();
-	}
-
-	// Driver Code
-	public static void main(String[] args)
-	{
-		int[] arr = { 10, 7, 8, 9, 1, 5 };
-		int n = arr.length;
-
-		quickSort(arr, 0, n - 1);
-		System.out.println("Sorted array: ");
-		printArray(arr, n);
-	}
+public class QuickSort {
+    public static void main(String[] args) {
+        for(int n = 10;n<=50;n+=5){
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++) arr[i] = i;
+            int[] cnt = {0};
+            long startT = System.nanoTime();
+            quickSort(arr,0,n-1,cnt);
+            long endT = System.nanoTime();
+            System.out.println(cnt[0] + "," + (endT - startT));
+        }
+    }
+    public static void quickSort(int[] arr,int s,int e,int[] cnt){
+        if(s < e){
+            int pi = partition(arr,s,e);
+            cnt[0]++;
+            quickSort(arr, s, pi-1,cnt);
+            cnt[0]++;
+            quickSort(arr, pi+1, e,cnt);
+        }
+    }
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static int partition(int[] arr,int s,int e){
+        int pivot = arr[e];
+        int i = s-1;
+        for(int j=s;j<e;j++){
+            if(arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,e);
+        return i+1;
+    }
 }
