a42c563cf3c6822146323c9b4bc9aceabeb33dd5
AbhishekKolhe <113412717+AbhishekKolhe@users.noreply.github.com>
2022-10-10 13:58:29 +0530

    bubblesort

--- bubblesort1.java
+++ bubblesort1.java
@@ -1,52 +1,33 @@
-// Optimized java implementation
-// of Bubble sort
-import java.io.*;
+// Bubble sort Implementation using Javascript
 
-class GFG
-{
-	// An optimized version of Bubble Sort
-	static void bubbleSort(int arr[], int n)
-	{
-		int i, j, temp;
-		boolean swapped;
-		for (i = 0; i < n - 1; i++)
-		{
-			swapped = false;
-			for (j = 0; j < n - i - 1; j++)
-			{
-				if (arr[j] > arr[j + 1])
-				{
-					// swap arr[j] and arr[j+1]
-					temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-					swapped = true;
-				}
-			}
 
-			// IF no two elements were
-			// swapped by inner loop, then break
-			if (swapped == false)
-				break;
-		}
-	}
-
-	// Function to print an array
-	static void printArray(int arr[], int size)
-	{
-		int i;
-		for (i = 0; i < size; i++)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-
-	// Driver program
-	public static void main(String args[])
-	{
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-		int n = arr.length;
-		bubbleSort(arr, n);
-		System.out.println("Sorted array: ");
-		printArray(arr, n);
+// Creating the bblSort function
+function bblSort(arr){
+	
+for(var i = 0; i < arr.length; i++){
+	
+// Last i elements are already in place
+for(var j = 0; j < ( arr.length - i -1 ); j++){
+	
+	// Checking if the item at present iteration
+	// is greater than the next iteration
+	if(arr[j] > arr[j+1]){
+		
+	// If the condition is true then swap them
+	var temp = arr[j]
+	arr[j] = arr[j + 1]
+	arr[j+1] = temp
 	}
 }
+}
+// Print the sorted array
+console.log(arr);
+}
+
+
+// This is our unsorted array
+var arr = [234, 43, 55, 63, 5, 6, 235, 547];
+
+
+// Now pass this array to the bblSort() function
+bblSort(arr);
