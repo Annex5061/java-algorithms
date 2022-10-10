cd531c33f57607d1c22e3829d56837a0dc41611b
pradip-424 <106142586+pradip-424@users.noreply.github.com>
2022-10-10 11:28:13 +0530

    Add files via upload

--- heap_sort.java
+++ heap_sort.java
@@ -1,74 +1,84 @@
-// Java program for implementation of Heap Sort
+import java.util.*;
 
 public class heap_sort {
-	public void sort(int arr[])
-	{
-		int N = arr.length;
+    public static void main(String[] args) {
 
-		// Build heap (rearrange array)
-		for (int i = N / 2 - 1; i >= 0; i--)
-			heapify(arr, N, i);
+        // int a[]={78,22,54,25,65};
+        // int n=a.length;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=n-i;
+        }
 
-		// One by one extract an element from heap
-		for (int i = N - 1; i > 0; i--) {
-			// Move current root to end
-			int temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
+      long start=System.nanoTime();
+      
+       heap_sort hp=new heap_sort();
+        hp.sort(a);
+       long end=System.nanoTime();
+       long runningtime=(end-start);
+        System.out.println("after sorting:" +"running time is"+runningtime +"ns");
+        // for(int i=0;i<n;i++)
+        // {
+        //     System.out.println(a[i]+ "");
+        // }
+        // System.out.println();
 
-			// call max heapify on the reduced heap
-			heapify(arr, i, 0);
-		}
-	}
+    }
 
-	// To heapify a subtree rooted with node i which is
-	// an index in arr[]. n is size of heap
-	void heapify(int arr[], int N, int i)
-	{
-		int largest = i; // Initialize largest as root
-		int l = 2 * i + 1; // left = 2*i + 1
-		int r = 2 * i + 2; // right = 2*i + 2
+ 
+void max_heapify(int a[],int n,int i)
+    {
+        int largest=i;
+        int l=2 * i+ 1;
+        int r=2 * i+ 2;
+     
+        if(l < n  && a[l] >  a[largest])
 
-		// If left child is larger than root
-		if (l < N && arr[l] > arr[largest])
-			largest = l;
+        {
+            largest=l;
+        }
+        
+        if(r < n && a[r] > a[largest])
 
-		// If right child is larger than largest so far
-		if (r < N && arr[r] > arr[largest])
-			largest = r;
+        {
+            largest=r;
+        }
+        if(largest!=i)
+     {
+             int temp=a[i];
+            a[i]=a[largest];  
+            a[largest]=temp;
 
-		// If largest is not root
-		if (largest != i) {
-			int swap = arr[i];
-			arr[i] = arr[largest];
-			arr[largest] = swap;
+            max_heapify(a, n, largest);
 
-			// Recursively heapify the affected sub-tree
-			heapify(arr, N, largest);
-		}
-	}
+        }
 
-	/* A utility function to print array of size n */
-	static void printArray(int arr[])
-	{
-		int N = arr.length;
+       
+        
 
-		for (int i = 0; i < N; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
 
-	// Driver's code
-	public static void main(String args[])
-	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
-		int N = arr.length;
+    }
+ public void sort(int a[])
+    {
+        int n=a.length;
+        for(int i=n / 2 - 1;i>=0;i--)
+        {
+            max_heapify(a, n, i);
+        }
+        for(int i=n-1; i>0;i--)
+        {
+         int t=a[0];
+         a[0]=a[i];
+         a[i]=t;
 
-		// Function call
-		heap_sort ob = new heap_sort();
-		ob.sort(arr);
+        max_heapify(a, i, 0);
+        }
 
-		System.out.println("Sorted array is");
-		printArray(arr);
-	}
+    }
+    
+  
+   
 }
