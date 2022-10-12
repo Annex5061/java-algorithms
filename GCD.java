c6c93ad61990a86b4545eb9f302dceecd26b469e
bhattji007 <shubhambhattrocks123@gmail.com>
2022-10-12 15:00:29 +0530

    hacktober fest 2022

--- GCD.java
+++ GCD.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
-public class GCDOfTwoNumbers {
-   public static void main(String args[]){
-      int a, b, i, hcf = 0;
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter first number :: ");
-      a = sc.nextInt();
-      System.out.println("Enter second number :: ");
-      b = sc.nextInt();
-
-      for(i = 1; i <= a || i <= b; i++) {
-         if( a%i == 0 && b%i == 0 )
-         hcf = i;
-      }
-      System.out.println("HCF of given two numbers is ::"+hcf);
-   }
-}
+import java.util.*;  
+public class LargestInArrayExample2{  
+public static int getLargest(Integer[] a, int total){  
+List<Integer> list=Arrays.asList(a);  
+Collections.sort(list);  
+int element=list.get(total-1);  
+return element;  
+}  
+public static void main(String args[]){  
+Integer a[]={1,2,5,6,3,2};  
+Integer b[]={44,66,99,77,33,22,55};  
+System.out.println("Largest: "+getLargest(a,6));  
+System.out.println("Largest: "+getLargest(b,7));  
+}} 
--- Main.java
+++ Main.java
@@ -1,35 +1,50 @@
-// Java program for implementation of Bubble Sort
-class BubbleSort {
-	void bubbleSort(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n - 1; i++)
-			for (int j = 0; j < n - i - 1; j++)
-				if (arr[j] > arr[j + 1]) {
-					// swap arr[j+1] and arr[j]
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-	}
-
-	/* Prints the array */
-	void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-
-	// Driver method to test above
-	public static void main(String args[])
-	{
-		BubbleSort ob = new BubbleSort();
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-		ob.bubbleSort(arr);
-		System.out.println("Sorted array");
-		ob.printArray(arr);
-	}
-}
-/* This code is contributed by Rajat Mishra */
+public class Main {  
+        public static void main(String[] args) {  
+            Main Obj = new Main();  
+            Obj.add(11);  
+            Obj.add(22);  
+            Obj.add(33);  
+            Obj.add(44); 
+			Obj.add(55); 
+            Obj.print();  
+        }  
+        public class Node{  
+            int element;  
+            Node next;  
+            public Node(int element) {  
+                this.element = element;  
+            }  
+        }  
+          
+        public Node head = null;  
+        public Node tail = null;  
+          
+        public void add(int element){  
+            Node newEle = new Node(element);  
+            if(head == null) {  
+                head = newEle;  
+                tail = newEle;  
+                newEle.next = head;  
+            }  
+            else {  
+                tail.next = newEle;  
+                tail = newEle;  
+                tail.next = head;  
+            }  
+        }  
+          
+        public void print() {  //print function
+            Node current = head;  
+            if(head == null) {  
+                System.out.println("List is empty");  
+            }  
+            else {  
+                System.out.println("Nodes of the circular linked list: ");  
+                 do{  
+                    System.out.print(" "+ current.element);  
+                    current = current.next;  
+                }while(current != head);  
+                System.out.println();  
+            }  
+        }  
+    }
